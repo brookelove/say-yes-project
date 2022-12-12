0385a8c21749306dfd120a7e9274e286a8b80303
brookelove <brookelove@Brookes-iMac-2.hsd1.wa.comcast.net>
2022-12-11 23:05:35 -0800

    create a file and start pseudocoding

---
+++ main.py
@@ -0,0 +1,9 @@
+
+'''
+1. welcome user to the say yes project
+2. then prompt user for their desired wedding date 
+    - inform them that they can change the date later 
+    - TRY / ELSE BLOCK where i am trying ot get a valid date that is greater than the current day
+    - while loop to ask for a valid date until it is not needs to be in a particular format MM/DD/YYYY and compare that it has to be greater than current date
+    
+'''
