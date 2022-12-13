92f468cdc38add984dec1b791be4cb35ac263d42
brookelove <brookelove@Brookes-iMac-2.hsd1.wa.comcast.net>
2022-12-12 23:31:26 -0800

    adding in streamlit inputs

--- main.py
+++ main.py
@@ -14,7 +14,33 @@
 5. output a file of the list of elements that the person has added 
 '''
 import streamlit as st
-
-st.write("Welcome to the Say Yes Project")
-st.write("Where you can keep tracj of all of your ideas for upcoming Nuptules!")
+from datetime import date
+st.title("Welcome to the Say Yes Project")
+st.subheader(
+    "Where you can keep track of all of your ideas for upcoming Nuptules!")
+st.button(label="YES")
 # print("Welcome to the Say Yes Project, where you can keep track of all of your ideas for your wedding!")
+
+user_name = st.text_input("What is Your name?")
+print(user_name)
+partner_name = st.text_input("What is your Partner's name?")
+print(partner_name)
+
+today = date.today()
+wedding_date = st.date_input(
+    "What is your projected date you want to get married? (YYYY/MM/DD)", min_value=today)
+wedding_date.strftime("%d/%m/%y")
+location = st.text_input(
+    "Do you know where you want to get married?")
+
+print(wedding_date)
+# ---------  this is where the table begins to get generated
+st.button(label="NEXT")
+# make a list of options thhey can pick on in a while loop until they click the word generate my list!
+
+random_thoughts = st.text_area("What are some of your random thoughts?")
+st.button("ENTER MY PLACES")
+
+list_options = st.selectbox(
+    'What category fits your inspiration?', ['Venue', 'Dress', 'Food', 'Flowers', 'Colors', 'Desserts'])
+done = st.button("IM DONE")
